b9e09f1f9770237c96c9cf3926ee464b51a595cc
Rosco Verheij <roscoverheij@Roscos-MacBook-Pro.local>
2018-08-23 11:42:58 +1000

    Built core functionality, can now delete tasks and move then from one area to another

--- taskFlow.js
+++ taskFlow.js
@@ -1,13 +1,57 @@
-const addTaskInput = $("#add-task");
+const taskInput = $("#add-task");
+const startArea = $("#start-area-list");
+const inProgressArea = $("#inProgress-area-list");
+const doneArea = $("#done-area-list");
 
-$(addTaskInput).on("keypress", function(event) {
+taskInput.on("keypress", function(event) {
     if(event.which === 13) {
         addTask();
     }
 })
 
 function addTask() {
-    let taskText = addTaskInput.val();
-    $("#start-area-list").append(`<li>${taskText}</li>`);
-    addTaskInput.val("");
+    const taskText = $("#add-task").val();
+    startArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${taskText}</li>`);
+    taskInput.val("");
 }
+
+startArea.on("click", "li", function() {
+    let startTask = $(this).text();
+    $(this).fadeOut(300, function() {
+        inProgressArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${startTask}</li>`);
+        $(this).remove();
+    })
+});
+
+
+startArea.on("click", "span", function(event){
+	$(this).parent().fadeOut(300,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+inProgressArea.on("click", "li", function() {
+    let inProgressTask = $(this).text();
+    $(this).fadeOut(300, function() {
+        doneArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${inProgressTask}</li>`);
+        $(this).remove();
+    })
+});
+
+inProgressArea.on("click", "span", function(event){
+	$(this).parent().fadeOut(300,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+
+
+
+
+
+
+ 
+
+
