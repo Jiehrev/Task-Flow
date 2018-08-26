3e07a840e92082de0a2a98781c5a0665380f4777
Rosco Verheij <roscoverheij@Roscos-MacBook-Pro.local>
2018-08-26 10:25:33 +1000

    Small update

--- taskFlow.js
+++ taskFlow.js
@@ -9,7 +9,7 @@ taskInput.on("keypress", function(event) {
     }
 })
 
-function addTask() {
+const addTask = function () {
     const taskText = $("#add-task").val();
     startArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${taskText}</li>`);
     taskInput.val("");
@@ -25,11 +25,12 @@ startArea.on("click", "li", function() {
 
 
 startArea.on("click", "span", function(event){
-	$(this).parent().fadeOut(300,function(){
+    $(this).parent().fadeOut(300,function(){
 		$(this).remove();
 	});
-	event.stopPropagation();
+    event.stopPropagation();
 });
+
 
 inProgressArea.on("click", "li", function() {
     let inProgressTask = $(this).text();
@@ -43,14 +44,14 @@ inProgressArea.on("click", "span", function(event){
 	$(this).parent().fadeOut(300,function(){
 		$(this).remove();
 	});
-	event.stopPropagation();
+    event.stopPropagation();
 });
 
 doneArea.on("click", "span", function(event){
 	$(this).parent().fadeOut(300,function(){
 		$(this).remove();
 	});
-	event.stopPropagation();
+    event.stopPropagation();
 });
 
 
