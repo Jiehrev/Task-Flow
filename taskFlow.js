259aa2a0b3cfef4bbb1b1a9ef9cff1745a3e3581
Rosco Verheij <roscoverheij@Roscos-MacBook-Pro.local>
2018-09-01 22:07:35 +1000

    Added progress bar feature and changed styling

--- taskFlow.js
+++ taskFlow.js
@@ -2,6 +2,9 @@ const taskInput = $("#add-task");
 const startArea = $("#start-area-list");
 const inProgressArea = $("#inProgress-area-list");
 const doneArea = $("#done-area-list");
+const progressBar = $("#progress-bar");
+let totalTasks = 0;
+let doneTasks = 0;
 
 taskInput.on("keypress", function(event) {
     if(event.which === 13) {
@@ -13,6 +16,7 @@ const addTask = function () {
     const taskText = $("#add-task").val();
     startArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${taskText}</li>`);
     taskInput.val("");
+    totalTasks++;
 }
 // Moves a task from the start area to the inprogress area when clicked
 startArea.on("click", "li", function() {
@@ -20,14 +24,17 @@ startArea.on("click", "li", function() {
     $(this).fadeOut(300, function() {
         inProgressArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${startTask}</li>`);
         $(this).remove();
+        updateProgress();
     })
 });
 // Deletes a task from the start area when clicked
 startArea.on("click", "span", function(event){
     $(this).parent().fadeOut(300,function(){
-		$(this).remove();
+        $(this).remove();
+        totalTasks--;
 	});
     event.stopPropagation();
+    updateProgress();
 });
 // Moves a task from the inprogress area to the done area when clicked
 inProgressArea.on("click", "li", function() {
@@ -35,19 +42,32 @@ inProgressArea.on("click", "li", function() {
     $(this).fadeOut(300, function() {
         doneArea.append(`<li class="task"><span><i class="fas fa-trash-alt"></i></span>${inProgressTask}</li>`);
         $(this).remove();
+        doneTasks++;
+        updateProgress();
     })
 });
 // Deletes a task from the inprogress area when clicked
 inProgressArea.on("click", "span", function(event){
 	$(this).parent().fadeOut(300,function(){
-		$(this).remove();
+        $(this).remove();
+        totalTasks--;
 	});
     event.stopPropagation();
 });
 // Deletes a task from the done area when clicked
 doneArea.on("click", "span", function(event){
 	$(this).parent().fadeOut(300,function(){
-		$(this).remove();
+        $(this).remove();
+        totalTasks--;
+        doneTasks--;
+        updateProgress();
 	});
     event.stopPropagation();
 });
+
+function updateProgress() {
+  let progress = 0;  
+  progress = (doneTasks / totalTasks) * 100;
+  progressBar.css("width", progress + "%");
+  progressBar.text(progress + "%"); 
+}
